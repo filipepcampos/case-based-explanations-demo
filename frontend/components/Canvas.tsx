663ddf18ed9b20e39a1d76d132a83e62cfbb3257
Filipe Campos <filipepcampos24@gmail.com>
2024-09-26 15:11:22 +0200

    fix: remove logo from Submit button

--- frontend/components/Canvas.tsx
+++ frontend/components/Canvas.tsx
@@ -184,13 +184,6 @@ export function Canvas({ setResult }: { setResult: (result: PredictionType) => v
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-30"
           onClick={submitCanvas}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/vercel.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
           Submit{" "}
         </button>
         <button
