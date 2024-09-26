43bbb723162b4fe9fdfca19cc2fbb423caf4549b
Filipe Campos <filipepcampos24@gmail.com>
2024-09-26 15:01:04 +0200

    fix: update github repo url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/filipepcampos"
+          href="https://github.com/filipepcampos/case-based-explanations-demo"
           target="_blank"
           rel="noopener noreferrer"
         >
