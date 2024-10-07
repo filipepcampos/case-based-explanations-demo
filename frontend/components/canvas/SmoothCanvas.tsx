3ea51a1e57b36be26ad34c3024abae86823ca051
Filipe Campos <filipepcampos24@gmail.com>
2024-10-07 12:44:31 +0200

    fix: reactivate dark mode

--- frontend/components/canvas/SmoothCanvas.tsx
+++ frontend/components/canvas/SmoothCanvas.tsx
@@ -136,7 +136,7 @@ export function SmoothCanvas({
       />
 
       <button
-        className="mt-4 mr-6 md:col-start-2 rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-30"
+        className="mt-4 mr-6 md:col-start-2 rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-white dark:bg-white dark:text-black gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-30"
         onClick={submitCanvas}
       >
         Submit{" "}
